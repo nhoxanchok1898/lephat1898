5227e84a3565bb09ad2cc047415c97741715c969
lephat1898 <lephat1898@gmail.com>
2026-02-01 23:39:26 +0700

    Fix store/models.py - restore all Django models

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Brand(models.Model):
